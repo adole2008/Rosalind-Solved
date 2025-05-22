a3e81a40f29b75663c552c73d7db0523a4394c84
adole2008 <doleanushka@gmail.com>
2025-05-21 20:20:30 -0400

    Solved Translating RNA to Protein

--- translating_rna_to_protein.py
+++ translating_rna_to_protein.py
@@ -1,22 +1,39 @@
 #Solution to Translating RNA to Protein (link: https://rosalind.info/problems/prot/)
 
-# UUU F      CUU L      AUU I      GUU V
-# UUC F      CUC L      AUC I      GUC V
-# UUA L      CUA L      AUA I      GUA V
-# UUG L      CUG L      AUG M      GUG V
-# UCU S      CCU P      ACU T      GCU A
-# UCC S      CCC P      ACC T      GCC A
-# UCA S      CCA P      ACA T      GCA A
-# UCG S      CCG P      ACG T      GCG A
-# UAU Y      CAU H      AAU N      GAU D
-# UAC Y      CAC H      AAC N      GAC D
-# UAA Stop   CAA Q      AAA K      GAA E
-# UAG Stop   CAG Q      AAG K      GAG E
-# UGU C      CGU R      AGU S      GGU G
-# UGC C      CGC R      AGC S      GGC G
-# UGA Stop   CGA R      AGA R      GGA G
-# UGG W      CGG R      AGG R      GGG G 
+codon_to_amino = {
+    "UUU" : "F", "UUC" : "F",
+    "UUA" : "L", "UUG" : "L", "CUU" : "L", "CUC" : "L", "CUG" : "L", "CUA" : "L",
+    "AUU" : "I", "AUC" : "I", "AUA" : "I",
+    "AUG" : "M",
+    "GUU" : "V", "GUC" : "V", "GUA" : "V", "GUG" : "V",
+    "UCU" : "S", "UCC" : "S", "UCA" : "S", "UCG" : "S", "AGU" : "S", "AGC" : "S",
+    "CCU" : "P", "CCC" : "P", "CCA" : "P", "CCG" : "P",
+    "ACU" : "T", "ACC" : "T", "ACA" : "T", "ACG" : "T",
+    "GCU" : "A", "GCC" : "A", "GCA" : "A", "GCG" : "A",
+    "UAU" : "Y", "UAC" : "Y",
+    "UAA" : "Stop", "UAG" : "Stop", "UGA" : "Stop",
+    "CAU" : "H", "CAC" : "H",
+    "CAA" : "Q", "CAG" : "Q",
+    "AAU" : "N", "AAC" : "N",
+    "AAA" : "K", "AAG" : "K",
+    "GAU" : "D", "GAC" : "D",
+    "GAA" : "E", "GAG" : "E",
+    "UGU" : "C", "UGC" : "C",
+    "UGG" : "W",
+    "CGU" : "R", "CGC" : "R", "CGA" : "R", "CGG" : "R", "AGA" : "R", "AGG" : "R",
+    "GGU" : "G", "GGC" : "G", "GGA" : "G", "GGG" : "G"
+}
 
-#strat: use a dict, same as Create task
-#don't want to do this rn cuz i already did something very similar in create task, would just be very similar
-#will complete later
+def main():
+    with open("example.txt", "r") as f:
+        rna_str = f.read()
+
+    translated = ""
+    for codon in range(0,len(rna_str), 3):
+        sub = rna_str[codon: codon+3]        
+        translated += codon_to_amino[sub]
+
+    print(translated)
+
+if __name__ == "__main__":
+    main()
