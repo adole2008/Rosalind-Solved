4880c0128eee0ef481f94c49496b01b2dd5e3de4
adole2008 <doleanushka@gmail.com>
2025-05-24 16:07:26 -0400

    Solved calculating expected offspring

--- calculating_expected_offspring.py
+++ calculating_expected_offspring.py
@@ -1,9 +1,16 @@
 #Solution for Calculating Expected Offspring (link: https://rosalind.info/problems/iev/)
 
-# plug into expected value formula
+# plug into expected value formula, every couple has exactly 2 offspring
 
 def main():
-    pass
+    with open("example.txt", "r") as f:
+        str = f.read().strip()
+        arr = list(map(int, str.split(" ")))
+
+    print(calc_expected_value(arr))
+
+def calc_expected_value(arr):
+    return arr[0] * 2.0 + arr[1] * 2.0 + arr[2] * 2.0 + arr[3] * 0.75 * 2.0 + arr[4]
 
 if __name__ == "__main__":
     main()
